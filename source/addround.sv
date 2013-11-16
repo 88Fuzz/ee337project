e073068da7a5cc0f21adb3476c8fde0e57551153
Mentor Graphics <mg69@ecelinux02.ecn.purdue.edu>
2013-11-16 18:48:48 -0500

    Revised controller and first try addroundkey

---
+++ source/addround.sv
@@ -0,0 +1,20 @@
+module addround
+(
+	input wire clk,
+	input wire n_rst,
+	input wire around_enable,
+	input wire [7:0] subkey,
+	input wire [7:0] olddata,
+	output reg [7:0] newdata
+);
+
+  assign newdata = (!n_rst) ? ((around_enable) ? (subkey ^ olddata) : olddata) : '0;
+  
+  /*always@(posedge clk, negedge n_rst) begin
+    if(n_rst == 1'b0) begin
+      newdata <= '0;
+    end else begin
+      newdata <= subkey ^ olddata;
+    end
+  end*/
+endmodule
