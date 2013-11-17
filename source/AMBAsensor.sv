7932b14f6fc2278fa7c1c93247c8fe96b9760579
Mentor Graphics <mg49@ecelinux03.ecn.purdue.edu>
2013-11-16 19:50:51 -0500

    WE NEED SRAM

--- source/AMBAsensor.sv
+++ source/AMBAsensor.sv
@@ -3,11 +3,11 @@ module AMBAsensor
   input wire clk,
   input wire n_rst,
   input wire [31:0] HADDR,
-  input wire [2,0] HBURST,
+  input wire [2:0] HBURST,
   input wire HMASTLOCK,
-  input wire [3,0] HPORT,
-  input wire [2,0] HSIZE,
-  input wire [1,0] HTRANS,
+  input wire [3:0] HPORT,
+  input wire [2:0] HSIZE,
+  input wire [1:0] HTRANS,
   input wire HWRITE,//high=write, low=read
   input wire HSELx,
   input wire HREADY,
@@ -23,4 +23,6 @@ localparam addr=32'hF0F0F0F0;
 assign addrMatch = (addr==HADDR) ? 1'b1 : 1'b0;
 assign mWrite=HWRITE;
 assign mRead=~HWRITE;
+assign dataReady=HREADY;
 
+endmodule
